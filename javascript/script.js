81978d6fe9b02c939ef777e3db4ebbb9ebd51004
Mariayolanda Delgado <mariayolanda.delgado@srburns.pe>
2019-11-17 21:35:12 -0500

    MAquetación de saludo y perfiles de usuario.

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,27 @@
 $( document ).ready(function() {
-	console.log('Hola mundo')
+	//console.log('Hola mundo')
+
+	var inputValidate = function () {
+      // validate phone input
+      $('body').find('#name').on('keydown', function(e) {
+		$('body').find('.pl-greeting__button').addClass('active')
+      })
+	}
+
+	if (window.location.pathname === '/profile.html') {
+		$('.pl-profile__slider').slick({
+			dots: true,
+			infinite: true,
+			speed: 300,
+			slidesToShow: 1,
+			prevArrow: '<img src="./img/icon-prev.png" alt="Power Like - Entel" class="pl-profile__slider__arrow prev-arrow">',
+			nextArrow: '<img src="./img/icon-next.png" alt="Power Like - Entel" class="pl-profile__slider__arrow next-arrow">'
+		});
+	}
+	
+	var powerLike = function () {
+		inputValidate()
+	}
+
+	powerLike()
 })
