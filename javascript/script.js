b821473172716582303d81002c84a3a950283b1a
willy.robles <willy.robles@srburns.pe>
2019-11-19 15:53:42 -0500

    Se re estructuró js a modular

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,71 @@
-$( document ).ready(function() {
-	//console.log('Hola mundo')
-
-	var inputValidate = function () {
-      // validate phone input
-      $('body').find('#name').on('keydown', function(e) {
-		$('body').find('.pl-greeting__button').addClass('active')
-      })
-	}
-
-	if (window.location.pathname === '/profile.html') {
-		$('.pl-profile__slider').slick({
-			dots: true,
-			infinite: true,
-			speed: 300,
-			slidesToShow: 1,
-			prevArrow: '<img src="./img/icon-prev.png" alt="Power Like - Entel" class="pl-profile__slider__arrow prev-arrow">',
-			nextArrow: '<img src="./img/icon-next.png" alt="Power Like - Entel" class="pl-profile__slider__arrow next-arrow">'
-		});
-	}
+let index = {
+	nameInput : function() {
+		$('#nameInput').on('keyup', function(e) {
+			$('.pl-greeting__button').addClass('active')
+			$('.pl-greeting__button').attr('href','welcome.html?user=' + $('#nameInput').val())
 	
-	var powerLike = function () {
-		inputValidate()
+			if($('#nameInput').val() === '') {
+				$('.pl-greeting__button').removeClass('active')
+				$('.pl-greeting__button').attr('href','javascript:void()')
+			}
+		})
 	}
+}
 
-	powerLike()
+let welcome = {
+	printName : function() {
+		$('#nameSpan').html(getParameterURL('user'))
+	}
+}
+
+let profile = {
+	loadSlider : function() {
+		if (window.location.pathname === '/profile.html') {
+			$('.pl-profile__slider').slick({
+				dots: true,
+				infinite: true,
+				speed: 300,
+				slidesToShow: 1,
+				prevArrow: '<img src="./img/icon-prev.png" alt="Power Like - Entel" class="pl-profile__slider__arrow prev-arrow">',
+				nextArrow: '<img src="./img/icon-next.png" alt="Power Like - Entel" class="pl-profile__slider__arrow next-arrow">'
+			})
+		}
+	}
+}
+
+let getParameterURL = function (parameter) {
+	let pageUrl = decodeURIComponent(window.location.search.substring(1)),
+		parameters = pageUrl.split('&'),
+		parameterName,
+		i
+
+	for (i = 0; i < parameters.length; i++) {
+		parameterName = parameters[i].split('=')
+
+		if (parameterName[0] === parameter) {
+			return parameterName[1] === undefined ? true : parameterName[1]
+		}
+	}
+}
+
+let load_index = function () {
+	index.nameInput()
+}
+
+let load_welcome = function () {
+	welcome.printName()
+}
+
+let load_profile = function () {
+	profile.loadSlider()
+}
+
+function initialize() {
+	load_index()
+	load_welcome()
+	load_profile()
+}
+
+$( document ).ready(function() {
+	initialize()
 })
